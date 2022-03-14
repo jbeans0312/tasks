69825b5fcdb949210f8160e9ef811666d1083686
John Bean <johnbean@wifi-roaming-128-4-68-178.wifi.udel.edu>
2022-03-14 11:44:47 -0400

    Finished CycleHoliday.tsx (untested)

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,48 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+export type Holiday =
+    | "🥚" //Egg Festival
+    | "🌸" //Flower Dance
+    | "🍲" //Luau
+    | "🎡" //Stardew Valley Fair
+    | "⛄"; //Feast of The Winter Star
+
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [currentHoliday, cycle] = useState<Holiday>("🥚");
+
+    const byAlphabet: Record<Holiday, Holiday> = {
+        "🎡": "🥚",
+        "🥚": "⛄",
+        "⛄": "🌸",
+        "🌸": "🍲",
+        "🍲": "🎡"
+    };
+
+    const bySeason: Record<Holiday, Holiday> = {
+        "⛄": "🥚",
+        "🥚": "🌸",
+        "🌸": "🍲",
+        "🍲": "🎡",
+        "🎡": "⛄"
+    };
+
+    return (
+        <div>
+            <div>
+                <Button onClick={() => cycle(byAlphabet[currentHoliday])}>
+                    Advance by Alphabet
+                </Button>
+                <Button onClick={() => cycle(bySeason[currentHoliday])}>
+                    Advance by Season
+                </Button>
+            </div>
+            <div>
+                <span>Holiday: {currentHoliday}</span>
+            </div>
+        </div>
+    );
 }
+/* Egg Festival (Early Spring), Flower Dance (Late Spring), Luau (Early Summer)
+ * Stardew Valley Fair (Mid Fall), Feast of The Winter Star (Late Winter)
+ */
