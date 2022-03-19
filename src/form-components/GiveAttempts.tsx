fd663999871396bdf6febb1fe937b87137d5ab1b
John Bean <johnbean@wifi-roaming-128-4-68-178.wifi.udel.edu>
2022-03-18 22:16:45 -0400

    Finished GiveAttempts.tsx (untested)

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,76 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+
+interface AttemptProps {
+    setRemaining: (request: number) => void;
+    remainingAttempts: number;
+    requestedAttempts: number;
+}
+
+function UseButton({
+    setRemaining,
+    remainingAttempts
+}: AttemptProps): JSX.Element {
+    return (
+        <Button
+            onClick={() => setRemaining(remainingAttempts - 1)}
+            disabled={remainingAttempts === 0}
+        >
+            Use
+        </Button>
+    );
+}
+
+function AddButton({
+    setRemaining,
+    remainingAttempts,
+    requestedAttempts
+}: AttemptProps): JSX.Element {
+    return (
+        <Button
+            onClick={() => setRemaining(remainingAttempts + requestedAttempts)}
+        >
+            Add
+        </Button>
+    );
+}
 
 export function GiveAttempts(): JSX.Element {
+    const [remainingAttempts, setRemaining] = useState<number>(3);
+    const [requestedAttempts, setRequested] = useState<number>(0);
+
+    function updateRequest(event: React.ChangeEvent<HTMLInputElement>) {
+        const userInput = parseInt(event.target.value);
+        if (!isNaN(userInput)) {
+            setRequested(userInput);
+        }
+    }
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            <div>
+                <span>{remainingAttempts}</span>
+                <Form.Group controlId="updateRequestAttempts">
+                    <Form.Label>Request Attempts</Form.Label>
+                    <Form.Control
+                        value={requestedAttempts}
+                        onChange={updateRequest}
+                    />
+                </Form.Group>
+            </div>
+            <div>
+                <UseButton
+                    setRemaining={setRemaining}
+                    remainingAttempts={remainingAttempts}
+                    requestedAttempts={requestedAttempts}
+                ></UseButton>
+                <AddButton
+                    setRemaining={setRemaining}
+                    remainingAttempts={remainingAttempts}
+                    requestedAttempts={requestedAttempts}
+                ></AddButton>
+            </div>
         </div>
     );
 }
